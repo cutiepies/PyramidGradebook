af06b605fbdff1ff4d4d0ea36565c0e099930259
Laury <laurr.jones@gmail.com>
2019-04-16 13:22:20 -0500

    added teacher
    teacher config

--- testpyramid.py
+++ testpyramid.py
@@ -7,18 +7,27 @@ from pyramid.response import Response
 def hello_world(request):
     return Response('Hello World!')
 
+def teacher(request):
+    return Response('<body><h3>Welcome to the Teacher Portal</h3></body>')
+
 def login(request):
     print('Incoming request')
     return Response('<body><h1>Welcome to Gradebook!</h1><h3>Please login with your email!<br><button type="button">Login!</button></h3></body>')
 
 def student(request):
-    return Response('<body></body>')
+    return Response('<body><h3>Welcome to the Student Portal</h3></br><h5>Your classes are displayed below</h5></body>')
 if __name__ == '__main__':
     with Configurator() as config:
+        #index login config
         config.add_route('login', '/')
         config.add_view(login, route_name='login')
-        config.add_route('hello', '/student')
-        config.add_view(hello_world, route_name='hello')
+        #student config
+        config.add_route('student', '/student')
+        config.add_view(student, route_name='student')
+        #teacher config
+        config.add_route('teacher', '/teacher')
+        config.add_view(teacher, route_name='teacher')
+
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
